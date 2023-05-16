8d09c945856a07212036ad96f5f0077c4db40afe
Hari Veerendra <veerendrahari396@gmail.com>
2023-05-16 17:40:16 +0530

    introduced a player to the game. when clicking on a square show either x or o

--- tictactoe/src/components/Board.jsx
+++ tictactoe/src/components/Board.jsx
@@ -3,18 +3,23 @@ import Square from './square';
 
 const Board = () => {
   const [square, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClick = clickedPosition => {
+    if (square[clickedPosition]) {
+      return;
+    }
     setSquares(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : 'O';
         }
         return squareValue;
       });
     });
-  };
 
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
   const renderSquare = position => {
     return (
       <Square
